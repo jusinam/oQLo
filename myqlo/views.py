e1250eed6818305aac1fda547d1d78a0c642e53d
DjCooGie <evansonchagwa01@gmail.com>
2020-02-10 01:58:01 +0300

    update the home view function in order to display comments

--- myqlo/views.py
+++ myqlo/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.templatetags.static import static
+from django.conf import settings
+from django.shortcuts import render, render_to_response, HttpResponseRedirect
 import datetime as dt
-from .models import Image
+from .models import Profile, Image, Comment
 from django.http import HttpResponse, Http404
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from .forms import NewImageForm, NewCommentForm, ProfileUpdateForm, RegisterForm
+from django.contrib import messages
+from .email import send_welcome_email
 
 
 
@@ -9,6 +16,20 @@ from django.http import HttpResponse, Http404
 def home(request):
     date = dt.date.today()
     images = Image.get_images()
-   
+    comments = Comment.get_comment()
+    
+    current_user = request.user 
+    if request.method == 'POST':
+        form = NewCommentForm(request.POST, auto_id=False)
+        img_id = request.POST['image_id']
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = current_user
+            image = Image.get_image(img_id)
+            comment.image = image
+            comment.save()
+        return redirect(f'/#{img_id}',)
+    else:
+        form = NewCommentForm(auto_id=False)
 
-    return render(request, 'index.html', {"date": date, "images":images})
+    return render(request, 'index.html', {"date": date, "images":images, "comments":comments, "form": form,})
