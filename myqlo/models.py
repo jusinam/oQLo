83964156ace77f20e1bd8c2ee02be56936c5ada5
DjCooGie <evansonchagwa01@gmail.com>
2020-02-10 01:30:08 +0300

    add decorator functions for search,image retrieve,likes and author(user)

--- myqlo/models.py
+++ myqlo/models.py
@@ -10,6 +10,10 @@ class Image(models.Model):
     image_file = models.ImageField(upload_to = 'images/', default='images/qlo.jpg')
     caption = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
+    Author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    author_profile = models.ForeignKey(Profile,on_delete=models.CASCADE, blank=True, default='1')
+    likes = models.ManyToManyField(User, related_name = 'likes', blank = True)
+
 
 
     def __str__(self):
@@ -21,3 +25,39 @@ class Image(models.Model):
     def delete_image(self):
         self.delete()
         
+    @classmethod
+    def get_images(cls):
+        images = cls.objects.all()
+        return images
+    
+    @classmethod
+    def get_by_author(cls, Author):
+        images = cls.objects.filter(Author=Author)
+        return images
+    
+    def total_likes(self):
+        self.likes.count()
+
+    @classmethod
+    def search_images(cls, search_term):
+        images = cls.objects.filter(description__icontains=search_term)
+        return images
+    
+    @classmethod
+    def get_image(request, id):
+        try:
+            image = Image.objects.get(pk = id)
+            print(image)
+            
+        except ObjectDoesNotExist:
+            raise Http404()
+        
+        return image
+    
+    def __str__(self):
+        return self.image_name
+    
+    class Meta:
+        ordering = ['pub_date']
+        verbose_name = 'Image'
+        verbose_name_plural = 'Images'
