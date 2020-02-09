edca793ade26a366dd3383dba47d4290cea9df56
DjCooGie <evansonchagwa01@gmail.com>
2020-02-09 20:08:27 +0300

    create home view function

--- myqlo/views.py
+++ myqlo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+import datetime as dt
+from .models import Image
+from django.http import HttpResponse, Http404
+
+
 
 # Create your views here.
+def home(request):
+    date = dt.date.today()
+    images = Image.get_images()
+   
+
+    return render(request, 'index.html', {"date": date, "images":images})
