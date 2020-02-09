d70ecd9ea78f09fd2139394e95f7823eb0823489
DjCooGie <evansonchagwa01@gmail.com>
2020-02-09 21:32:22 +0300

    create Imagetest class responsible for testing Image model class and add setup and is_instance test functions

--- myqlo/tests.py
+++ myqlo/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Image
 
 # Create your tests here.
+
+class ImageTestCase(TestCase):
+    
+    def setUp(self):
+        
+        # Set up method
+        self.new_image= Image(image_name = 'Qlo', image_file ='media/images/qlo.jpg', caption ='QLo is a chatting app')
+       
+
+     # Testing  instance
+    def test_is_instance(self):
+        self.assertTrue(isinstance(self.new_image,Image))
