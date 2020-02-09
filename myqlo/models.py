233de47a20801a64940d4768433db9ec4f1662c8
DjCooGie <evansonchagwa01@gmail.com>
2020-02-10 01:47:50 +0300

    create Profile model

--- myqlo/models.py
+++ myqlo/models.py
@@ -1,9 +1,35 @@
 from django.db import models
 import datetime as dt
+from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 from PIL import Image
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    photo = models.ImageField(upload_to = 'profiles/', blank=True, default='profiles/qlo.jpg')
+
+    def save_profile(self):
+        self.save()
+        
+        img = Image.open(self.photo.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.photo.path)
+
+    def delete_profile(self):
+        self.delete()
+    
+    def __str__(self):
+        return f'self.user, self.bio, self.photo'
+    
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+
+
          
 class Image(models.Model):
     image_name = models.CharField(max_length=255)
