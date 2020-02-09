b2c358b41bf0e388299558203c086d3d04bcd4c3
DjCooGie <evansonchagwa01@gmail.com>
2020-02-09 21:34:16 +0300

    create image delete test function

--- myqlo/tests.py
+++ myqlo/tests.py
@@ -20,3 +20,11 @@ class ImageTestCase(TestCase):
         self.new_image.save_image()
         all_objects = Image.objects.all()
         self.assertTrue(len(all_objects)>0)
+
+     # Testing  deleting
+    def test_delete_method(self):
+        self.new_image.save_image()
+        filtered_object = Image.objects.filter(image_name='Qlo')
+        Image.delete_image(filtered_object)
+        all_objects = Image.objects.all()
+        self.assertTrue(len(all_objects) == 0)
