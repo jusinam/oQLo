7c0025426af4206fec96d69725a601d640c4c4eb
DjCooGie <evansonchagwa01@gmail.com>
2020-02-09 20:09:37 +0300

    create Image model class with four fields

--- myqlo/models.py
+++ myqlo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import datetime as dt
+from tinymce.models import HTMLField
+from PIL import Image
 
 # Create your models here.
+         
+class Image(models.Model):
+    image_name = models.CharField(max_length=255)
+    image_file = models.ImageField(upload_to = 'images/', default='images/qlo.jpg')
+    caption = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
