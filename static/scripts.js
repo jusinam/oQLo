8732e2275783508338ec071ce03e95264b817e5b
DjCooGie <evansonchagwa01@gmail.com>
2020-02-11 02:02:53 +0300

    add script code, styles and favicons

---
+++ static/scripts.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+
+    $('#btn-one').click(function() {
+        $('#btn-one').html('<span class="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span>Loading...')
+      });
+
+  
+})
