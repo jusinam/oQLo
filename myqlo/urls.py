16151414c0fe7f4bc9a7aae7ad909036ebb41b40
DjCooGie <evansonchagwa01@gmail.com>
2020-02-10 16:20:46 +0300

    include image results, new image, accounts and like roots

--- myqlo/urls.py
+++ myqlo/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls import url
+from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
+
 
 urlpatterns = [
-    url(r'^$', views.home, name= 'home')
+    url(r'^$', views.home, name= 'home'),
+    url(r'^search/', views.search_images, name='search_results'),
+    url(r'^image/(\d+)', views.get_image, name='image_results'),
+    url(r'^new/image$', views.new_image, name='new-image'),
+    url(r'^accounts/profile/$', views.user_profiles, name='profile'),
+    url(r'^like/(\d+)', views.like_image, name='like_image'),
+
 ]
 
 if settings.DEBUG:
