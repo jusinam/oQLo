35711a7e93d42ac862381fd71d247615474dc217
DjCooGie <evansonchagwa01@gmail.com>
2020-02-10 16:19:02 +0300

    modify model imports

--- myqlo/forms.py
+++ myqlo/forms.py
@@ -1,6 +1,6 @@
 from crispy_forms.helper import FormHelper
 from django import forms
-from .models import Profile, Image, Comment
+from .models import Image, Comment, Profile
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from registration.forms import RegistrationForm
